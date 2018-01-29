53c5d253461938e030de737da4a80f91cdb03bf9
Jan Trommelmans <jan.trommelmans@me.com>
2018-01-29 10:36:00 +0100

    Added probability calculation for democrats

--- JT.ProbCalc.R
+++ JT.ProbCalc.R
@@ -1,8 +1,12 @@
 # Function to translate qualifications to probabilities
 JT.ProbCalc <- function(JT.vec) {
-  if (grepl("Safe", JT.vec)) {prob <- 0.95}
-  if (grepl("Like", JT.vec)) {prob <- 0.85}
-  if (grepl("Lean", JT.vec)) {prob <- 0.75}
-  if (grepl("Toss", JT.vec)) {prob <- 0.5}
+  prob <- numeric(length(JT.vec))
+  for (i in 1:length(JT.vec)) {
+    if (grepl("Safe", JT.vec[i])) {prob[i] <- 0.95}
+    if (grepl("Like", JT.vec[i])) {prob[i] <- 0.85}
+    if (grepl("Lean", JT.vec[i])) {prob[i] <- 0.75}
+    if (grepl("Tilt", JT.vec[i])) {prob[i] <- 0.60}
+    if (grepl("Toss", JT.vec[i])) {prob[i] <- 0.5}
+  }
   return(prob)
 }
